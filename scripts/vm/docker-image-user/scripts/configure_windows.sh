5447337bd87245416ad8d8d5a66bb246a6b726d0
Joe Robinson <joseph.robinson@krminc.com>
2018-05-11 15:02:41 -0400

    Scritp Update * Pointing configure_windows script to Github

--- scripts/vm/docker-image-user/scripts/configure_windows.sh
+++ scripts/vm/docker-image-user/scripts/configure_windows.sh
@@ -3,10 +3,10 @@
 vista_machine=$1
 org=$2
 
-vm_name_base="itcpWin-${org}"
+vm_name_base="vitcWin-${org}"
 vm_name=$(echo $vm_name_base | awk '{print substr($0,0,15)}')
 
-FILE_URI="https://raw.githubusercontent.com/OSEHRA/itcp-scripts/scripts/vm/docker-image-user/ps-scripts/configureWindows.ps1"
+FILE_URI="https://raw.githubusercontent.com/OSEHRA/VistA-in-the-Cloud/blob/master/scripts/vm/docker-image-user/ps-scripts/configureWindows.ps1"
 echo "Configuring Windows VM"
 echo "**Warning** This can take about 15 minutes"
 az vm extension set --publisher Microsoft.Compute --name CustomScriptExtension --version 1.8 --settings "{\"fileUris\": [\"$FILE_URI\"], \"commandToExecute\":\"powershell.exe -ExecutionPolicy Unrestricted -File .\\configureWindows.ps1 $vista_machine \"}" --vm-name $win_vm_name --resource-group $vm_group_name > /dev/null
