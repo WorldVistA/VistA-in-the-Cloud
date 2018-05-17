d976b6c951c1f12d8008d7a6be0e49e4c737ee2e
Joe Robinson <joseph.robinson@krminc.com>
2018-05-17 11:39:20 -0400

    Sandbox Template Update * Added Linux deploy and configure * Added shell version of provision_rhel_machine for template to run

---
+++ scripts/provision_rhel_machine.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yum install -y yum-utils device-mapper-persistent-data lvm2 git && yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && yum install -y docker-ce && mkdir -p /mnt/resource/docker /etc/docker && echo '{\"data-root\": \"/mnt/resource/docker\"}' > /etc/docker/daemon.json && systemctl start docker && cd /mnt/resource && docker pull osehra/osehravista && docker run -p 9430:9430 -p 8001:8001 -p9080:9080 -p2222:22 -p57772:57772 -d -P --name=cache osehra/osehravista
