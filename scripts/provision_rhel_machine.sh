16eb3f73c56b83d0a9eb2622ad6117b1c6b91264
Christopher Edwards <Christopher.Edwards@krminc.com>
2018-07-06 17:53:12 -0400

    Update docker command to support PIP SQL Mapping
    PIP SQL Mapping requires another port forward, 61012, and increased
    sysctl sized for messages: kernel.msgmax=1048700 and kernel.msgmnb=65536000
    
    This doesn't affect applications that don't listen on port 61012
    or need increased sysctl message sizes

--- scripts/provision_rhel_machine.sh
+++ scripts/provision_rhel_machine.sh
@@ -10,4 +10,4 @@ echo '{"data-root": "/mnt/resource/docker"}' > /etc/docker/daemon.json
 systemctl start docker
 cd /mnt/resource
 docker pull osehra/osehravista
-docker run -p 9430:9430 -p 8001:8001 -p9080:9080 -p2222:22 -p57772:57772 -d -P --name=cache osehra/osehravista
+docker run -p 9430:9430 -p 8001:8001 -p9080:9080 -p2222:22 -p57772:57772 -p 61012:61012 -d -P --sysctl kernel.msgmax=1048700 --sysctl kernel.msgmnb=65536000 --name=cache osehra/osehravista
